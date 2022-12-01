504a7126177afbe5f618fc9b133fd9f7c86f723b
juanigm <juanigm99@gmail.com>
2022-12-01 15:10:57 -0300

    New Test gha

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
