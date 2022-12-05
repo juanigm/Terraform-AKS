a8bd95b5ad0420af85e758844b6e13c40301855b
juanigm <juanigm99@gmail.com>
2022-12-05 14:40:45 -0300

    TestA

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-
 # Azure Provider source and version being used
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-tfstates"
     storage_account_name = "statestfashe"
     container_name       = "tfstatedevops"
-    key = "sp=racwd&st=2022-12-05T17:37:30Z&se=2022-12-06T01:37:30Z&spr=https&sv=2021-06-08&sr=c&sig=lhsD4txjB3nsO3AG1jhcL8TzTNQJx%2FVfTWXVyTJ4rss%3D"
+    key                  = "sp=racwd&st=2022-12-05T17:37:30Z&se=2022-12-06T01:37:30Z&spr=https&sv=2021-06-08&sr=c&sig=lhsD4txjB3nsO3AG1jhcL8TzTNQJx%2FVfTWXVyTJ4rss%3D"
   }
 
   required_providers {
