4e79535d6b19680c2c15497c67205ae2c5185685
juanigm <juanigm99@gmail.com>
2022-12-14 10:58:14 -0300

    Testkubectl

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 
 # Azure Provider source and version being used
 terraform {
-  /*backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "terraform-tfstates"
     storage_account_name = "statestfashe"
     container_name       = "tfstatedevops"
     key                  = "prod.terraform.tfstate"
-  } */
+  } 
 
   required_providers {
     azurerm = {
