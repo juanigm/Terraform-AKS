22ae293246e972265b1cd444236ce2182b073f28
juanigm <juanigm99@gmail.com>
2023-01-10 10:26:33 -0300

    Test Final

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-# Azure Provider source and version being used
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-tfstates"
-    storage_account_name = "statestfashe"
-    container_name       = "tfstatedevops"
-    key                  = "prod.terraform.tfstate"
-  } 
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
 resource "azurerm_resource_group" "aks-rg" {
   name     = var.resource_group_name
   location = var.location
--- providers.tf
+++ providers.tf
@@ -1,8 +1,23 @@
 # Configure the Microsoft Azure Provider
+
+# Azure Provider source and version being used
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-tfstates"
+    storage_account_name = "statestfashe"
+    container_name       = "tfstatedevops"
+    key                  = "prod.terraform.tfstate"
+  } 
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
